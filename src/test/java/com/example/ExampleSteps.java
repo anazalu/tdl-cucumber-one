bba05536cba49c5c43166af60f4e77288bea5e99
anazalu <you@example.com>
2024-04-21 19:40:00 +0300

    add more test steps

--- src/test/java/com/example/ExampleSteps.java
+++ src/test/java/com/example/ExampleSteps.java
@@ -4,45 +4,65 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ExampleSteps {
     private final WebDriver driver = WebDriverConfig.getDriver();
+    private By productsLinkBy = By.xpath("//a[@href='/products']");
 
     @Given("I am on the Automation Exercise front page")
     public void i_am_on_the_automation_exercise_page() {
         driver.get("https://www.automationexercise.com/");
     }
 
-    @When("I do nothing")
-    public void searchFor() {
-    }
+    // @Then("the page title should be {string}")
+    // public void checkTitle(String titleText) {
+    //     new WebDriverWait(driver, Duration.ofSeconds(10)).until(new ExpectedCondition<Boolean>() {
+    //         public Boolean apply(WebDriver d) {
+    //             return d.getTitle().equals(titleText);
+    //         }
+    //     });
+    // }
 
     @Then("the page title should be {string}")
     public void checkTitle(String titleText) {
-        new WebDriverWait(driver, Duration.ofSeconds(10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().equals(titleText);
-            }
-        });
+        String actualTitle = driver.getTitle();
+        String expectedTitle = titleText;
+        Assert.assertEquals(actualTitle, expectedTitle, "Title mismatch");
     }
 
-    @Then("the page should contain {string}")
-    public void checkProductsFake(String titleText) {
-        new WebDriverWait(driver, Duration.ofSeconds(10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().equals(titleText);
-            }
-        });
+    @Then("the Products link should contain text {string}")
+    public void checkProductsLinkText(String linkText) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement productsLink = wait.until(ExpectedConditions.visibilityOfElementLocated(productsLinkBy));
+        String actualLinkText = productsLink.getText();
+        String expectedLinkTextFragment = linkText;
+        Assert.assertTrue(actualLinkText.contains(expectedLinkTextFragment), "Link text mismatch");
+    }
+
+    @When("I click Products link")
+    public void clickProductsLink() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement productsLink = wait.until(ExpectedConditions.visibilityOfElementLocated(productsLinkBy));
+        productsLink.click();
+    }
+
+    @Then ("the Products page opens that has {string} header")
+    public void verifyLocationIsProductsPage(String headerText) {
+        By allProductsHeaderBy = By.xpath("/html/body/section[2]/div[1]/div/div[2]/div/h2");        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement allProductsHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(allProductsHeaderBy));
+        String actualHeaderText = allProductsHeader.getText().toLowerCase();
+        String expectedHeaderText = headerText.toLowerCase();
+        Assert.assertEquals(actualHeaderText, expectedHeaderText, "Header text mismatch");
     }
 
     @After()
--- src/test/resources/features/example.feature
+++ src/test/resources/features/example.feature
@@ -1,8 +1,19 @@
 Feature: Automation Exercise front page
   Scenario: Check the title
     Given I am on the Automation Exercise front page
-    When I do nothing
     Then the page title should be "Automation Exercise"
-    And the page should contain "Automation Exercise"
-    
-    
+    And the Products link should contain text "Products"
+    When I click Products link
+    Then the Products page opens that has "All Products" header
+
+  # Scenario Outline: Adding products to Cart
+  #   Given I am on Products page
+  #   When I add <items> 
+  #   Then I should have <items> in Cart
+
+  #   Examples:
+  #     | items        | 
+  #     |    Blue top  |
+  #     |    Green top |
+  #     |    Red top   |
+  
